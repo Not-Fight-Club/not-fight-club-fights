183f201a00dd820f6bde7d0a760e6328acd3ea30
Jonathan Bukowsky <jonathan.bukowsky@revature.net>
2021-10-05 14:48:29 -0400

    Update project references, add models project, and scaffold dbcontext from database

---
+++ FightsApi_Data/Vote.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace FightsApi_Data
+{
+    public partial class Vote
+    {
+        public int VoteId { get; set; }
+        public int FightId { get; set; }
+        public int FighterId { get; set; }
+        public int UserId { get; set; }
+
+        public virtual Fight Fight { get; set; }
+        public virtual Fighter Fighter { get; set; }
+    }
+}
