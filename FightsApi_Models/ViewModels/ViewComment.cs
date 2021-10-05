3b721ac1d2651e625eefc8b10de84f4c2514e870
Jonathan Bukowsky <jonathan.bukowsky@revature.net>
2021-10-05 15:15:11 -0400

    Initial changes to facilitate importing repository and controller code

---
+++ FightsApi_Models/ViewModels/ViewComment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotFightClub_Models.ViewModels
+{
+    class ViewComment
+    {
+        //public Comment()
+        //{
+        //    InverseParentcommentNavigation = new HashSet<Comment>();
+        //}
+
+        public int CommentId { get; set; }
+        public int? FightId { get; set; }
+        public Guid? UserId { get; set; }
+        public DateTime? Date { get; set; }
+        public string Comment1 { get; set; }
+        public int? Parentcomment { get; set; }
+
+        //public virtual Fight Fight { get; set; }
+        //public virtual Comment ParentcommentNavigation { get; set; }
+        //public virtual UserInfo User { get; set; }
+        //public virtual ICollection<Comment> InverseParentcommentNavigation { get; set; }
+    }
+}
---
+++ FightsApi_Models/ViewModels/ViewWager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotFightClub_Models.ViewModels
+{
+    class ViewWager
+    {
+        public int WagerId { get; set; }
+        public Guid? UserId { get; set; }
+        public int? FightId { get; set; }
+        public int? Amount { get; set; }
+        public int? FighterId { get; set; }
+
+        //public virtual Fight Fight { get; set; }
+        //public virtual Fighter Fighter { get; set; }
+        //public virtual UserInfo User { get; set; }
+    }
+}
