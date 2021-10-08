90500b1ecdc96491eabc7183eba96eba78fe34e3
Jonathan Bukowsky <jonathan.bukowsky@revature.net>
2021-10-08 12:16:52 -0400

    Updates to mock for dbcontext

--- FightsApi_Test/RepositoryTests/FightsRepoTest/FightRepoTest.cs
+++ FightsApi_Test/RepositoryTests/FightsRepoTest/FightRepoTest.cs
@@ -2,6 +2,7 @@
 using FightsApi_Data;
 using FightsApi_Logic.Mappers;
 using FightsApi_Models.ViewModels;
+using FightsApi_Test.RepositoryTests.DatabaseMock;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,69 +12,69 @@ using Xunit;
 
 namespace FightsApi_Test.RepositoryTests.FightsRepoTest
 {
-	[Collection("Repository Tests")]
-	public class FightRepoTest
+  [Collection("Repository Tests")]
+  public class FightRepoTest
+  {
+    static Guid? z = new Guid();
+    public static IEnumerable<object[]> GetData()
     {
-		    static Guid? z = new Guid();
-			public static IEnumerable<object[]> GetData()
-			{
-			Fight test = new Fight();
-			
-			int? j = 1;
-			int? n = 1;
+      Fight test = new Fight();
 
-			
-				for (var i = 1; i <= 1000; i++)
-				{
-					yield return new object[] { i, z, DateTime.Now, DateTime.Now, j, n};
-				}
-			}
+      int? j = 1;
+      int? n = 1;
 
-			[Theory]
-			[MemberData(nameof(GetData))]
-			public async Task Test_FightRepo(int id, Guid creatorId, DateTime startTime, DateTime endTime, int locationId, int weatherId)
-			{
 
-				// set up an in-memory version of NotFightClub
-				using (var mockDbContext = new P3_NotFightClubContext())
-				{
-					// Delete the database if it already exists
-					mockDbContext.Database.EnsureDeleted();
-					// Re-make the database
-					mockDbContext.Database.EnsureCreated();
+      for (var i = 1; i <= 1000; i++)
+      {
+        yield return new object[] { i, z, DateTime.Now, DateTime.Now, j, n };
+      }
+    }
 
-				var fight = new Fight()
-				{
-					FightId = id,
-					CreatorId = creatorId,
-					StartDate = startTime,
-				    EndDate = endTime,
-				    Location = locationId,
-					Weather = weatherId
-					};
-					mockDbContext.Fights.Add(fight);
-					mockDbContext.SaveChanges();
+    [Theory]
+    [MemberData(nameof(GetData))]
+    public async Task Test_FightRepo(int id, Guid creatorId, DateTime startTime, DateTime endTime, int locationId, int weatherId)
+    {
 
-				// TODO: set up a null logger and a mocked htttpClient
-				var fightmapper = new FightMapper();
-				var fightrepotest = new FightRepository(fightmapper, mockDbContext) ;
-				List<ViewFight> fighties = new List<ViewFight>();
-			     fighties= await fightrepotest.Read();
+      // set up an in-memory version of NotFightClub
+      using (var mockDbContext = FightsApiMock.GetDbContext())
+      {
+        // Delete the database if it already exists
+        mockDbContext.Database.EnsureDeleted();
+        // Re-make the database
+        mockDbContext.Database.EnsureCreated();
 
-					Assert.NotEmpty(fighties);
-					foreach (var element in fighties)
-					{
-						Assert.Equal(id, fight.FightId);
-						Assert.Equal(creatorId, fight.CreatorId);
-						Assert.Equal(startTime, fight.StartDate);
-						Assert.Equal(endTime, fight.EndDate);
-						Assert.Equal(locationId, fight.Location);
-					    Assert.Equal(weatherId, fight.Weather);
-					    
-				    }
+        var fight = new Fight()
+        {
+          FightId = id,
+          CreatorId = creatorId,
+          StartDate = startTime,
+          EndDate = endTime,
+          Location = locationId,
+          Weather = weatherId
+        };
+        mockDbContext.Fights.Add(fight);
+        mockDbContext.SaveChanges();
 
-				}
-			}
-		
-	}
+        // TODO: set up a null logger and a mocked htttpClient
+        var fightmapper = new FightMapper();
+        var fightrepotest = new FightRepository(fightmapper, mockDbContext);
+        List<ViewFight> fighties = new List<ViewFight>();
+        fighties = await fightrepotest.Read();
+
+        Assert.NotEmpty(fighties);
+        foreach (var element in fighties)
+        {
+          Assert.Equal(id, fight.FightId);
+          Assert.Equal(creatorId, fight.CreatorId);
+          Assert.Equal(startTime, fight.StartDate);
+          Assert.Equal(endTime, fight.EndDate);
+          Assert.Equal(locationId, fight.Location);
+          Assert.Equal(weatherId, fight.Weather);
+
+        }
+
+      }
+    }
+
+  }
 }
