d3804888e1fce01ac587049ceb25f010c2594089
13478 <13478@DESKTOP-CTAL6HT>
2021-10-18 12:56:58 -0400

    commited changes

---
+++ FightsApi/Controllers/WeatherController.cs
@@ -0,0 +1,30 @@
+﻿using FightsApi_Buisiness.Interfaces;
+using FightsApi_Buisiness.Repositories;
+using FightsApi_Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FightsApi.Controllers
+{
+	[Route("api")]
+	[ApiController]
+	public class WeatherController : Controller
+	{
+		private IWeatherRepository _wr;
+		public WeatherController(IWeatherRepository wr)
+		{
+			_wr = wr;
+		}
+
+		[HttpGet("[Controller]/")]
+		public async Task<ActionResult<List<ViewWeather>>> All()
+		{
+			List<ViewWeather> weathers = await _wr.Read();
+			return Ok(weathers);
+		}
+
+	}
+}
---
+++ FightsApi_Buisiness/Repositories/WeatherRepository.cs
@@ -0,0 +1,55 @@
+﻿using FightsApi_Data;
+using FightsApi_Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FightsApi_Buisiness.Interfaces;
+
+namespace FightsApi_Buisiness.Repositories
+{
+	public class WeatherRepository : IWeatherRepository
+	{
+		private readonly P3_NotFightClubContext _dbContext;
+    //get all weather from db to pick a random weather
+    public WeatherRepository(P3_NotFightClubContext dbContext)
+		{
+      _dbContext = dbContext;
+    }
+
+		public Task<ViewWeather> Add(ViewWeather obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<List<ViewWeather>> Read()
+    {
+      List<ViewWeather> allWeather = await _dbContext.Weathers
+        .Select(w => new ViewWeather
+        {
+          WeatherId = w.WeatherId,
+          Description = w.Description
+        }).ToListAsync();
+      return allWeather;
+    }
+		public async Task <ViewWeather> ReadRandom()
+		{
+			List<ViewWeather> weathers = await this.Read();
+			Random random = new Random();
+			int id = random.Next(0, weathers.Count() - 1);
+			return weathers[id];
+		}
+
+		public Task<ViewWeather> Read(int obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<ViewWeather> Update(ViewWeather obj)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
